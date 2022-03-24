b761ab4176fd3a1fed9a5668eeb5e2ced82ca958
Toby Anderson <tobyartisan@outlook.com>
2022-03-24 08:01:01 -0700

    Add proto file and gRPC code gen command.

---
+++ generate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# See the following for an example of these commands: https://grpc.io/docs/languages/go/quickstart/#regenerate-grpc-code
+
+# Using deprecated gRPC plugin
+#protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
+
+# Using new gRPC plugin
+protoc --go_out=. --go-grpc_out=. greet/greetpb/greet.proto
