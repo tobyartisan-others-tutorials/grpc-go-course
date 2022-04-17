1528796464094cf3812ac96000c707a2a6c50538
Toby Anderson <tobyartisan@outlook.com>
2022-04-17 17:56:04 -0600

    Removing shell script as it was replaced with Makefile.

--- generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# See the following for an example of these commands: https://grpc.io/docs/languages/go/quickstart/#regenerate-grpc-code
-
-# Using new gRPC plugin
-protoc -Igreet/proto --go_out=. --go_opt=module=github.com/tobyartisan-others-tutorials/grpc-go-course  --go-grpc_out=. --go-grpc_opt=module=github.com/tobyartisan-others-tutorials/grpc-go-course greet/proto/dummy.proto
